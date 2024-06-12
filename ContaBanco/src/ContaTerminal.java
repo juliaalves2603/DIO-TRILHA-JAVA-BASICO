7315412739670dec1ac5062a1d4ef4155998c019
juliaalves.gomes757@gmail.comgit confih --global user.email juliaalves.gomes757@gmail.com <juliaalves.gomes757@gmail.com>
2024-06-12 19:59:37 -0300

    ContaBanco

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -1,6 +1,13 @@
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+
+        // Exibir a mensagem para o nosso usuário
+        System.out.println("Bem Vindo, Julia");
+        System.out.println("Sua agencia é = 1234");
+        System.out.println("Sua conta é = 002-1");
+        // Exibir a mensagem conta criada seu Saldo é
+        System.out.println("Seu saldo disponivel é = 2000");
+        System.out.println("Obrigado por escolher nosso banco para chamar de seu");
 
     }
 }
